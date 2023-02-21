a9ed9403218e0ce96996b0eb54d63eaf92e3ce12
Nathaniel Graham <nathaniel.graham@protonmail.com>
2023-02-21 13:24:21 -0700

    command line

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,66 @@
 use std::collections::BTreeSet;
+use std::env;
 
 fn main() {
-    let size = 4;
-    let startpos = 12;
-    println!("Tile Positions
---------------
-0  1  2  3  4
-5  6  7  8  9
-10 11 12 13 14
-15 16 17 18 19
-20 21 22 23 24
---------------");
-    let mut explore_board: Vec<Tile> = vec![Tile::Empty;25];
-    explore_board[12] = Tile::Tile;
+    let args: Vec<String> = env::args().collect();
+    let mut startpos: usize;
+    let mut patternsize: usize = 1;
+    match args.len() {
+        // no args passed
+        1 => {
+            usage();
+            std::process::exit(0)},
+        2 => {
+            match args[1].parse() {
+                Ok(pos) => {startpos = pos;},
+                Err(err) => {
+                    println!("{}", err);
+                    usage();
+                    std::process::exit(0);
+                }
+            }
+        },
+        3 => {
+            match (args[1].parse(), args[2].parse()) {
+                (Ok(pos), Ok(size)) => {
+                    startpos = pos;
+                    patternsize = size;
+                },
+                (Ok(_), Err(err)) => {
+                    println!("{}", err);
+                    usage();
+                    std::process::exit(0);
+                },
+                (Err(err), Ok(_)) => {
+                    println!("{}", err);
+                    usage();
+                    std::process::exit(0);
+                },
+                (Err(err1), Err(err2)) => {
+                    println!("{}", err1);
+                    println!("{}", err2);
+                    usage();
+                    std::process::exit(0);
+                },
+            }
+        },
+        _ => {
+            println!("Too many arguments.");
+            usage();
+            std::process::exit(0);
+        }
+    }
     let mut pattern: Pattern = Pattern {
         tiles: BTreeSet::new(),
         frontiers: BTreeSet::new(),
         board: vec![Tile::Empty; 25],
     };
-    pattern.frontiers.insert(12);
+    pattern.frontiers.insert(startpos);
     let mut patterns: Patterns = Patterns::new();
     patterns.insert(pattern.clone());
 
     // for each size, until the desired size
-    for _ in 0..size {
+    for _ in 0..patternsize {
         let mut sizepatterns: Patterns = Patterns::new();
 
         // loop through each existing pattern of size n
@@ -42,6 +79,18 @@ fn main() {
         print_board(&display_board);
     }
 
+}
+
+fn usage() {
+    println!("USAGE: cargo run -- <startpos> <tilecount>
+Tile Positions
+--------------
+0  1  2  3  4
+5  6  7  8  9
+10 11 12 13 14
+15 16 17 18 19
+20 21 22 23 24
+--------------");
 }
 
 type Patterns = BTreeSet<Pattern>;
